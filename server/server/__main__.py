8ab505d60146f3329b6bbd0314fca994ea1aa275
Jim Madge <jmmadge@gmail.com>
2020-03-12 16:17:25 +0000

    Adjust API return value

--- server/server/__main__.py
+++ server/server/__main__.py
@@ -9,8 +9,8 @@ api = Api(app)
 def default_desk_status():
     status = {
         "status": "free",
-        "name": "",
-        "until": ""
+        "name": None,
+        "until": None
         }
     return status
 
@@ -20,7 +20,7 @@ desk_status = defaultdict(default_desk_status)
 
 class DeskStatus(Resource):
     def get(self, desk_id):
-        return {desk_id: desk_status[desk_id]}
+        return desk_status[desk_id]
 
 
 api.add_resource(DeskStatus, '/<string:desk_id>')
