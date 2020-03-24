e237af9463d4dd6935bcfe832ea8b4857e53f2d3
Jim Madge <jmmadge@gmail.com>
2020-03-24 12:05:34 +0000

    Add docstrings to app

--- server/server/__main__.py
+++ server/server/__main__.py
@@ -1,3 +1,4 @@
+"""Flask RESTful API for hotdesk indicators."""
 from collections import defaultdict
 from flask import Flask
 from flask_restful import Resource, Api
@@ -7,6 +8,7 @@ api = Api(app)
 
 
 def default_desk_status():
+    """Produce the default desk status entry."""
     status = {
         "status": "free",
         "name": None,
@@ -19,7 +21,20 @@ desk_status = defaultdict(default_desk_status)
 
 
 class DeskStatus(Resource):
+    """Desk status RESTful resource."""
+
     def get(self, desk_id):
+        """
+        Handle a get request to the DeskStatus resource.
+
+        This returns the current status of a desk.
+
+        :arg str desk_id: The unique identifier of the desk whose status should
+            be returned.
+
+        :returns: The status of desk `desk_id`.
+        :rtype: dict
+        """
         return desk_status[desk_id]
 
 
