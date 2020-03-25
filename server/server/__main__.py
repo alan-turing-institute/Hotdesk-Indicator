e8347c05e188a055b0144009638bbc4178c6f9c0
Jim Madge <jmmadge@gmail.com>
2020-03-25 14:47:19 +0000

    Add (very) basic form

--- server/server/__main__.py
+++ server/server/__main__.py
@@ -1,14 +1,21 @@
 """Flask RESTful API for hotdesk indicators."""
 from collections import defaultdict
+import datetime
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_restful import Resource, Api
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, DateTimeField, SelectField
+from wtforms.validators import DataRequired
 
+# Initial Flask app and extensions
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'xh)9l8;{P-us_&h>@q{u'
 api = Api(app)
 bootstrap = Bootstrap(app)
 
 
+# Construct desk status dictionary
 def default_desk_status():
     """Produce the default desk status entry."""
     status = {
@@ -20,14 +27,11 @@ def default_desk_status():
 
 
 desk_status = defaultdict(default_desk_status)
-
-
 desk_status['REG-42'] = {
     "status": "taken",
     "name": "Kaiser Söze",
     "until": None
     }
-
 desk_status['REG-07'] = {
     "status": "free",
     "name": "Kaiser Söze",
@@ -35,6 +39,7 @@ desk_status['REG-07'] = {
     }
 
 
+# Desk status RESTful API
 class DeskStatus(Resource):
     """Desk status RESTful resource."""
 
@@ -55,11 +60,46 @@ class DeskStatus(Resource):
 
 api.add_resource(DeskStatus, '/<string:desk_id>')
 
+# Forms
+desks = [
+    ('REG-07', 'REG-07'),
+    ('REG-42', 'REG-42'),
+]
 
+
+class BookingForm(FlaskForm):
+    """Desk booking form."""
+
+    name = StringField('Name', validators=[DataRequired()])
+    desk = SelectField('Desk', choices=desks, validators=[DataRequired()])
+    from_when = DateTimeField('From when', format="%H:%M",
+                              default=datetime.datetime.now(),
+                              validators=[DataRequired()])
+    until_when = DateTimeField('Until when', format="%H:%M",
+                               default=datetime.datetime.now(),
+                               validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+# Routes
 @app.route('/')
 def index():
     """Index route."""
     return render_template('index.html')
+
+
+@app.route('/book', methods=['GET', 'POST'])
+def book():
+    """Desk booking route."""
+    form = BookingForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        desk = form.desk.data
+        from_when = form.from_when.data
+        until_when = form.until_when.data
+        print(name, desk, from_when, until_when)
+        return render_template('index.html')
+    return render_template('book.html', form=form)
 
 
 if __name__ == '__main__':
