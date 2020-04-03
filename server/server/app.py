a878e5175bed2d91956dc44e8301879f739f6343
Jim Madge <jmmadge@gmail.com>
2020-04-03 10:12:39 +0100

    Add bookings database

--- server/server/app.py
+++ server/server/app.py
@@ -1,18 +1,48 @@
 """Flask RESTful API for hotdesk indicators."""
 from collections import defaultdict
 import datetime
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
+import os
 from wtforms import StringField, SubmitField, DateTimeField, SelectField
 from wtforms.validators import DataRequired
 
 # Initial Flask app and extensions
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'xh)9l8;{P-us_&h>@q{u'
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    )
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 api = Api(app)
 bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+
+
+# Define models(tables)
+class Desk(db.Model):
+    """Desk model."""
+
+    __tablename__ = 'desks'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(6), unique=True)
+    bookings = db.relationship('Booking', backref='desk', lazy='dynamic')
+
+
+class Booking(db.Model):
+    """Booking model."""
+
+    __tablename__ = 'bookings'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=False)
+    from_when = db.Column(db.Time, unique=False)
+    until_when = db.Column(db.Time, unique=False)
+    desk_id = db.Column(db.Integer, db.ForeignKey('desks.id'))
 
 
 # Construct desk status dictionary
@@ -93,15 +123,19 @@ def book():
     """Desk booking route."""
     form = BookingForm()
     if form.validate_on_submit():
-        name = form.name.data
-        desk = form.desk.data
-        from_when = form.from_when.data
-        until_when = form.until_when.data
-        print(name, desk, from_when.time(), until_when.time())
+        booking = Booking(
+            name=form.name.data,
+            desk_id=form.desk.data,
+            from_when=form.from_when.data.time(),
+            until_when=form.until_when.data.time()
+            )
+        db.session.add(booking)
+        db.session.commit()
         flash('Your desk is booked!')
         return redirect(url_for('index'))
     return render_template('book.html', form=form)
 
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True, host='0.0.0.0')
