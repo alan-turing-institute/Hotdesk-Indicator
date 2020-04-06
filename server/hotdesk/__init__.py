13af67e70e5068bdb02002e3e7bc01f08a5a4d5b
Jim Madge <jmmadge@gmail.com>
2020-04-06 15:17:34 +0100

    Restructure configuration

--- server/hotdesk/__init__.py
+++ server/hotdesk/__init__.py
@@ -1,21 +1,32 @@
 """App module."""
-from .config import configs
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+import os
+import pathlib
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 
 
-def create_app(config_name=None):
+def create_app(test_config=None):
     """Create Flask App instance."""
     app = Flask(__name__)
-    if config_name is None:
-        app.config.from_object(configs['development'])
+    app.config.from_mapping(
+        SECRET_KEY="uxh)9l8;{P-us_&h>@q{u",
+        SQLALCHEMY_DATABASE_URI=(
+            "sqlite:///" + os.path.join(app.instance_path, "data.sqlite")
+            ),
+        SQLALCHEMY_TRACK_MODIFICATIONS=False
+        )
+
+    pathlib.Path(app.instance_path).mkdir(exist_ok=True)
+
+    if test_config:
+        app.config.update(test_config)
     else:
-        app.config.from_object(configs[config_name])
+        app.config.from_envvar("FLASK_CONFIG", silent=True)
 
     bootstrap.init_app(app)
     db.init_app(app)
