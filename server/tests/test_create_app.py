6ff14d418ec5501a05f95d24767c6df5ded0bea4
Jim Madge <jmmadge@gmail.com>
2020-04-16 16:14:16 +0100

    Add tests for create_app factory

---
+++ server/tests/test_create_app.py
@@ -0,0 +1,27 @@
+"""Test the app factory."""
+from hotdesk import create_app
+
+
+def test_test_config(app):
+    assert app.config["TESTING"] is True
+    assert "data-test.sqlite" in app.config["SQLALCHEMY_DATABASE_URI"]
+    assert app.config["SECRET_KEY"] == "uxh)9l8;{P-us_&h>@q{u"
+
+
+def test_env_var(tmp_path, monkeypatch):
+    config_path = tmp_path / "config.py"
+    monkeypatch.setenv("FLASK_CONFIG", str(config_path))
+
+    db_path = tmp_path / "data-test-env.sqlite"
+
+    with open(config_path, "w") as config:
+        config.writelines([
+            "TESTING = True\n",
+            f"SQLALCHEMY_DATABASE_URI = 'sqlite:///' + '{str(db_path)}'\n"
+            ])
+
+    app = create_app()
+
+    assert app.config["TESTING"] is True
+    assert "data-test-env.sqlite" in app.config["SQLALCHEMY_DATABASE_URI"]
+    assert app.config["SECRET_KEY"] == "uxh)9l8;{P-us_&h>@q{u"
