15bfbecfa04f77aa8636bbec96186a9e3bc05c9d
Jim Madge <jmmadge@gmail.com>
2020-04-24 08:37:54 +0100

    Add missing docstrings

--- server/tests/test_create_app.py
+++ server/tests/test_create_app.py
@@ -3,12 +3,18 @@ from hotdesk import create_app
 
 
 def test_test_config(app):
+    """Test the default app created by the app fixture."""
     assert app.config["TESTING"] is True
     assert "data-test.sqlite" in app.config["SQLALCHEMY_DATABASE_URI"]
     assert app.config["SECRET_KEY"] == "uxh)9l8;{P-us_&h>@q{u"
 
 
 def test_env_var(tmp_path, monkeypatch):
+    """
+    Test creating an app using a configuration file.
+
+    This includes testing overwriting the default variables.
+    """
     config_path = tmp_path / "config.py"
     monkeypatch.setenv("FLASK_CONFIG", str(config_path))
 
